af0e54e9200cfd756216c303dd46a51b79df05c5
xDepcio <olek.drwal@gmail.com>
2024-12-07 15:42:01 +0100

    working udp client

--- riot-os/temp-sensor/main.c
+++ riot-os/temp-sensor/main.c
@@ -2,11 +2,11 @@
 #include <stdint.h>
 #include "periph/gpio.h"
 #include "dht11_module.h"
+#include "gnrc_udp.h"
 
 int main(void)
 {
     puts("Welcome to RIOT!\n");
-    puts("Type `help` for help, type `saul` to see all SAUL devices\n");
 
     gpio_t pin = GPIO_PIN(PORT_A, 4);
     uint8_t humidity_integral = 0;
@@ -15,6 +15,28 @@ int main(void)
     uint8_t temp_decimal = 0;
 
     read_dht(&pin, &humidity_integral, &humidity_decimal, &temp_integral, &temp_decimal);
+    char json_string[256];
+    /* json format:
+        * {
+        *     "humidity": {
+        *        "value": 0.0,
+        *        "unit": "percent"
+        *     },
+        *     "temperature": {
+        *        "value": 0.0,
+        *        "unit": "celsius"
+        *     }
+        * }
+    */
+    sprintf(
+        json_string,
+        "{\"humidity\": {\"value\": %d.%d, \"unit\": \"percent\"}, \"temperature\": {\"value\": %d.%d, \"unit\": \"celsius\"}}",
+        humidity_integral,
+        humidity_decimal,
+        temp_integral,
+        temp_decimal
+    );
+    gnrc_udp_send("fe80::2ef0:5dff:fe9e:fbdb", "8808", json_string, 1, 1000);
 
     printf("Humidity: %d.%d %%\n", humidity_integral, humidity_decimal);
     printf("Temperature: %d.%d °C\n", temp_integral, temp_decimal);
