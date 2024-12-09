6c6f9680bf14d3703fdd6f75c325f4a66f4551b6
xDepcio <olek.drwal@gmail.com>
2024-12-09 12:47:03 +0100

    riot change

--- riot-os/temp-sensor/main.c
+++ riot-os/temp-sensor/main.c
@@ -15,28 +15,18 @@ int main(void)
     uint8_t temp_decimal = 0;
 
     read_dht(&pin, &humidity_integral, &humidity_decimal, &temp_integral, &temp_decimal);
-    char json_string[256];
+    char json_temp[256];
+    char json_humidity[256];
+    sprintf(json_temp, "{\"sensor_id\": 1, \"value\": %d.%d}", temp_integral, temp_decimal);
+    sprintf(json_humidity, "{\"sensor_id\": 2, \"value\": %d.%d}", humidity_integral, humidity_decimal);
     /* json format:
         * {
-        *     "humidity": {
-        *        "value": 0.0,
-        *        "unit": "percent"
-        *     },
-        *     "temperature": {
-        *        "value": 0.0,
-        *        "unit": "celsius"
-        *     }
+        *     "sensor_id": <int>,
+        *     "value": <float>
         * }
     */
-    sprintf(
-        json_string,
-        "{\"humidity\": {\"value\": %d.%d, \"unit\": \"percent\"}, \"temperature\": {\"value\": %d.%d, \"unit\": \"celsius\"}}",
-        humidity_integral,
-        humidity_decimal,
-        temp_integral,
-        temp_decimal
-    );
-    gnrc_udp_send("fe80::2ef0:5dff:fe9e:fbdb", "8808", json_string, 1, 1000);
+    gnrc_udp_send("fe80::2ef0:5dff:fe9e:fbdb", "5000", json_temp, 1, 1000);
+    gnrc_udp_send("fe80::2ef0:5dff:fe9e:fbdb", "5000", json_humidity, 1, 1000);
 
     printf("Humidity: %d.%d %%\n", humidity_integral, humidity_decimal);
     printf("Temperature: %d.%d °C\n", temp_integral, temp_decimal);
