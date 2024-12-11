88becb73614b65d104ed540e178e67d69f6c53ce
jedrzej-grabski <grabski.dev@gmail.com>
2024-12-11 20:19:22 +0100

    Added fetching methods required for user-specific api requests.

--- backend/src/backend/utils/database_utils/fetching.py
+++ backend/src/backend/utils/database_utils/fetching.py
@@ -15,3 +15,71 @@ async def fetch_test_data():
         measurements = result.scalars().all()
         return measurements
 
+
+async def fetch_latest_measurements_for_platform(platform_id: int) -> dict:
+    async with async_session() as session:
+        # Subquery: Get the latest measurement date for each sensor on the platform
+        latest_measurements_dates_subquery = (
+            select(
+                Measurement.sensor_id,
+                func.max(Measurement.date).label("latest_date"),
+            )
+            .join(Sensor, Sensor.id == Measurement.sensor_id)
+            .filter(Sensor.platform_id == platform_id)
+            .group_by(Measurement.sensor_id)
+            .subquery()
+        )
+
+        # Main Query: Join with Measurement Type, and get latest measurements
+
+        query = (
+            select(
+                Measurement.sensor_id,
+                Measurement.value,
+                Measurement.date,
+                MeasurementType.physical_parameter,
+                MeasurementType.unit,
+            )
+            .join(
+                latest_measurements_dates_subquery,
+                (
+                    Measurement.sensor_id
+                    == latest_measurements_dates_subquery.c.sensor_id
+                )
+                & (
+                    Measurement.date == latest_measurements_dates_subquery.c.latest_date
+                ),
+            )
+            .join(Sensor, Sensor.id == Measurement.sensor_id)
+            .join(MeasurementType, MeasurementType.id == Sensor.measurement_type_id)
+        )
+
+        result = await session.execute(query)
+
+        rows = result.mappings().all()
+
+        return {
+            row["physical_parameter"]: {
+                "sensor_id": row["sensor_id"],
+                "value": row["value"],
+                "date": row["date"],
+                "unit": row["unit"],
+            }
+            for row in rows
+        }
+
+
+async def fetch_user_platform_access(user_id: int, platform_id: int) -> bool:
+    """
+    Check if a user has access to a specific platform.
+    """
+    async with async_session() as session:
+        query = (
+            select(UserPlatform)
+            .filter(
+                UserPlatform.user_id == user_id, UserPlatform.platform_id == platform_id
+            )
+            .limit(1)
+        )
+        result = await session.execute(query)
+        return result.scalars().first() is not None
