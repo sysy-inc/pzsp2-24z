1054e6e70ee7bbb67ba2b322fcc40d263726c2bc
jedrzej-grabski <grabski.dev@gmail.com>
2024-12-11 20:49:20 +0100

    typo

--- backend/src/backend/utils/auth_utils.py
+++ backend/src/backend/utils/auth_utils.py
@@ -1,6 +1,6 @@
 from src.backend.utils.database_utils.models import User
 
 
-def get_current_user() -> User:
+def get_current_user() -> int:
     """Authenticate the user and return the user's ID."""
     return 1  # Mocked user ID
