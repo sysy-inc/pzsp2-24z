e669f4a72a2631a9167559ca277f80f1186de9ee
jedrzej-grabski <grabski.dev@gmail.com>
2024-12-11 20:14:16 +0100

    Fixed database scripts to support auto-incrementaion of primary keys.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,15 +1,19 @@
-INSERT INTO measurement_types (id, physical_parameter, unit) VALUES
-    (1, 'Temperature', '°C'),
-    (2, 'Humidity', '%');
+INSERT INTO measurement_types (physical_parameter, unit) VALUES
+    ('Temperature', '°C'),
+    ('Humidity', '%');
 
-INSERT INTO users (id, name, surname, email, passwd) VALUES
-    (1, 'admin', 'admin', 'admin@admin', 'admin');
+INSERT INTO users (name, surname, email, passwd) VALUES
+    ('admin', 'admin', 'admin@admin', 'admin');
 
-INSERT INTO platforms (id, name) VALUES
-    (1, 'Example Platform');
+INSERT INTO platforms (name) VALUES
+    ('Example Platform');
 
-INSERT INTO users_platforms (id, user_id, platform_id) VALUES
-    (1, 1, 1);
+INSERT INTO users_platforms (user_id, platform_id) VALUES
+    (1, 1);
+
+INSERT INTO sensors (measurement_type_id, platform_id) VALUES
+    (1, 1),
+    (2, 1);
 
 INSERT INTO sensors (id, measurement_type_id, platform_id) VALUES
     (1, 1, 1),
