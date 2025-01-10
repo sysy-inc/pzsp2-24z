363e20bbf25df3274635d27a33fe687eae1d3108
xDepcio <olek.drwal@gmail.com>
2025-01-10 16:43:04 +0100

    added decorator to execute scripts within database before test starts

---
+++ backend/tests/api_tests/conftest.py
@@ -0,0 +1,55 @@
+from functools import wraps
+from typing import Callable
+import psycopg2
+
+
+def run_pg_query(
+    db_name: str,
+    db_user: str,
+    db_password: str,
+    db_host: str,
+    db_port: int,
+    query_file: str,
+):
+    connection = psycopg2.connect(
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port,
+        database=db_name,
+    )
+
+    cursor = connection.cursor()
+    with open(query_file, "r") as file:
+        query = file.read()
+        cursor.execute(query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def postgres_db_fixture(
+    db_name: str,
+    db_user: str,
+    db_password: str,
+    db_host: str,
+    db_port: int,
+    queries: list[str],
+):
+    def decorator(func: Callable[..., None]):
+        @wraps(func)
+        def wrapper():
+            for query in queries:
+                run_pg_query(
+                    db_name=db_name,
+                    db_user=db_user,
+                    db_password=db_password,
+                    db_host=db_host,
+                    db_port=db_port,
+                    query_file=query,
+                )
+            func()
+
+        return wrapper
+
+    return decorator
--- scripts/clear_db.sql
+++ scripts/clear_db.sql
@@ -1,29 +1,29 @@
 -- Drop all tables
 DO $$
 BEGIN
-    EXECUTE (
+    EXECUTE COALESCE((
         SELECT string_agg('DROP TABLE IF EXISTS ' || table_schema || '.' || table_name || ' CASCADE;', ' ')
         FROM information_schema.tables
         WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
-    );
+    ), '');
 END $$;
 
 -- Drop all views
 DO $$
 BEGIN
-    EXECUTE (
+    EXECUTE COALESCE((
         SELECT string_agg('DROP VIEW IF EXISTS ' || table_schema || '.' || table_name || ' CASCADE;', ' ')
         FROM information_schema.views
         WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
-    );
+    ), '');
 END $$;
 
 -- Drop all sequences
 DO $$
 BEGIN
-    EXECUTE (
+    EXECUTE COALESCE((
         SELECT string_agg('DROP SEQUENCE IF EXISTS ' || sequence_schema || '.' || sequence_name || ' CASCADE;', ' ')
         FROM information_schema.sequences
         WHERE sequence_schema NOT IN ('pg_catalog', 'information_schema')
-    );
+    ), '');
 END $$;
