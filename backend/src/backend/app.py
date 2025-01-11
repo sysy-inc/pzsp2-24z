55b8aa91789b58d1d2cb87054357e406630dd480
xDepcio <olek.drwal@gmail.com>
2025-01-11 18:49:08 +0100

    Added tests for udp server

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -1,7 +1,8 @@
-from contextlib import contextmanager
+from contextlib import asynccontextmanager, contextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from src.backend.utils.database_utils.db_controller import close_db, init_db
 from src.backend.routes.api import api_router
 from src.backend.routes.platforms import platforms
 from src.backend.udp_controller import init_udp_server
@@ -9,16 +10,16 @@ from src.backend.udp_controller import init_udp_server
 udp_server = None
 
 
-# @contextmanager
-# def lifespan(app: FastAPI):
-#     init_db()
-#     # init_udp_server()
-#     yield
-#     print("Shutting down")
-#     close_db()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    await init_udp_server()
+    yield
+    print("Shutting down")
+    close_db()
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router, prefix="/api")
 app.include_router(platforms, prefix="/api/platforms")
 
