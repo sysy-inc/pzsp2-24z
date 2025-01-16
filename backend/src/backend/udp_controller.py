abf30b7c5fb4b6a599fbe3ce6cda24840ce213f8
jedrzej-grabski <grabski.dev@gmail.com>
2025-01-16 13:17:59 +0100

    Added typing to UDP controller

--- backend/src/backend/udp_controller.py
+++ backend/src/backend/udp_controller.py
@@ -1,29 +1,29 @@
 import asyncio
-from typing import Callable
+from typing import Callable, Awaitable, Coroutine, Any, Optional
 from src.backend.utils.database_utils.storing import save_sample_to_db
 
 
 class UDPServer(asyncio.DatagramProtocol):
-    def __init__(self, save_to_db_callback: Callable[[str], asyncio.Task]):
+    def __init__(self, save_to_db_callback: Callable[[str], Coroutine[Any, Any, None]]):
         self.save_to_db = save_to_db_callback
 
-    def connection_made(self, transport):
+    def connection_made(self, transport: asyncio.transports.DatagramTransport) -> None:
         self.transport = transport
 
-    def datagram_received(self, data, addr):
+    def datagram_received(self, data: bytes, addr: tuple[str, int]) -> None:
         message = data.decode()
         print(f"Received message from {addr}: {message}")
 
         # Schedule the save task
         asyncio.create_task(self.save_to_db(message))
 
-    def connection_lost(self, exc):
+    def connection_lost(self, exc: Optional[Exception]) -> None:
         print("Connection lost")
 
 
-async def init_udp_server(host_addr="0.0.0.0", port="5000"):
+async def init_udp_server(host_addr: str = "0.0.0.0", port: str = "5000") -> None:
     loop = asyncio.get_event_loop()
-    transport, protocol = await loop.create_datagram_endpoint(
-        lambda: UDPServer(save_sample_to_db), local_addr=(host_addr, port)
+    transport, protocol = await loop.create_datagram_endpoint(  # type: ignore
+        lambda: UDPServer(save_sample_to_db), local_addr=(host_addr, port)  # type: ignore
     )
     print(f"UDP server listening on {host_addr}:{port}")
--- backend/src/backend/utils/auth_utils.py
+++
@@ -1,3 +0,0 @@
-def get_current_user() -> int:
-    """Authenticate the user and return the user's ID."""
-    return 1  # Mocked user ID
