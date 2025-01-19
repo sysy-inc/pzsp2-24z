f1ac9fc0695128010b63d5575e76f06ef422a954
xDepcio <olek.drwal@gmail.com>
2025-01-19 23:11:25 +0100

    changed udp ipv4 to ipv6

--- backend/src/backend/udp_controller.py
+++ backend/src/backend/udp_controller.py
@@ -22,7 +22,7 @@ class UDPServer(asyncio.DatagramProtocol):
         print("Connection lost")
 
 
-async def init_udp_server(host_addr: str = "0.0.0.0", port: str = "5000") -> None:
+async def init_udp_server(host_addr: str = "::", port: str = "5000") -> None:
     loop = asyncio.get_event_loop()
     transport, protocol = await loop.create_datagram_endpoint(  # type: ignore
         lambda: UDPServer(save_sample_to_db), local_addr=(host_addr, port)  # type: ignore
