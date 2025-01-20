45a0cd5abea6bb5868b2a3c63880e6447eca8dd8
mkrajew4 <01171632@pw.edu.pl>
2025-01-20 01:27:04 +0100

    got rid of time.h

--- riot-os/temp-sensor/main.c
+++ riot-os/temp-sensor/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h> // exit()
-#include <time.h>
+#include <sys/time.h>
 #include "periph/gpio.h"
 #include "dht11_module.h"
 #include "gnrc_udp.h"
@@ -10,52 +10,53 @@
 
 int main(void)
 {
-    puts("Welcome to RIOT!\n");
-    srand(time(NULL));
+        puts("Welcome to RIOT!\n");
+        uint32_t seed = ztimer_now(ZTIMER_MSEC);
+        srand(seed);
 
 #ifdef TEST_UDP_IPV6
-    int i = 0;
-    while (i < 5)
-    {
+        int i = 0;
+        while (i < 5)
+        {
 #endif
 #ifndef TEST
-        gpio_t pin = GPIO_PIN(PORT_A, 4);
+                gpio_t pin = GPIO_PIN(PORT_A, 4);
 #endif
-        uint8_t humidity_integral = 0;
-        uint8_t humidity_decimal = 0;
-        uint8_t temp_integral = 0;
-        uint8_t temp_decimal = 0;
+                uint8_t humidity_integral = 0;
+                uint8_t humidity_decimal = 0;
+                uint8_t temp_integral = 0;
+                uint8_t temp_decimal = 0;
 
 #ifndef TEST
-        read_dht(&pin, &humidity_integral, &humidity_decimal, &temp_integral, &temp_decimal);
+                read_dht(&pin, &humidity_integral, &humidity_decimal, &temp_integral, &temp_decimal);
 #endif
-        char json_temp[512];
-        char json_humidity[512];
-        size_t json_temp_len = 0;
-        size_t json_humidity_len = 0;
-        sprintf(json_temp, "{\"sensor_id\": 1, \"value\": %d.%d}", temp_integral, temp_decimal);
-        json_temp_len = encrypt(json_temp);
-        printf("%s\n", json_temp);
-        sprintf(json_humidity, "{\"sensor_id\": 2, \"value\": %d.%d}", humidity_integral, humidity_decimal);
-        json_humidity_len = encrypt(json_humidity);
-        // printf("%s\n", json_humidity);
-        /* json format:
-         * {
-         *     "sensor_id": <int>,
-         *     "value": <float>
-         * }
-         */
-        gnrc_udp_send(HOST_IPV6, HOST_PORT, json_temp, json_temp_len, 1, 1000);
-        gnrc_udp_send(HOST_IPV6, HOST_PORT, json_humidity, json_humidity_len, 1, 1000);
+                char json_temp[512];
+                char json_humidity[512];
+                size_t json_temp_len = 0;
+                size_t json_humidity_len = 0;
+                sprintf(json_temp, "{\"sensor_id\": 1, \"value\": %d.%d}", temp_integral, temp_decimal);
+                json_temp_len = encrypt(json_temp);
+                printf("%s\n", json_temp);
+                sprintf(json_humidity, "{\"sensor_id\": 2, \"value\": %d.%d}", humidity_integral, humidity_decimal);
+                json_humidity_len = encrypt(json_humidity);
+                // printf("%s\n", json_humidity);
+                /* json format:
+                 * {
+                 *     "sensor_id": <int>,
+                 *     "value": <float>
+                 * }
+                 */
+                gnrc_udp_send(HOST_IPV6, HOST_PORT, json_temp, json_temp_len, 1, 1000);
+                gnrc_udp_send(HOST_IPV6, HOST_PORT, json_humidity, json_humidity_len, 1, 1000);
 
-        printf("Humidity: %d.%d %%\n", humidity_integral, humidity_decimal);
-        printf("Temperature: %d.%d °C\n", temp_integral, temp_decimal);
+                printf("Humidity: %d.%d %%\n", humidity_integral, humidity_decimal);
+                printf("Temperature: %d.%d °C\n", temp_integral, temp_decimal);
 
 #ifdef TEST_UDP_IPV6
-        ztimer_sleep(ZTIMER_MSEC, 1000);
-        i++;
-    }
-    exit(0);
+                ztimer_sleep(ZTIMER_MSEC, 1000);
+                i++;
+        }
+        exit(0);
 #endif
-    return 1;
+        return 1;
 }
